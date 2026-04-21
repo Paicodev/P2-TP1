33e4eaef05e7cdc4a9f19c68e6bbedc4c649da77
Pablo Aguilar <paedmaag@gmail.com>
2026-04-21 01:23:52 -0300

    -m feat: Se completó el ejercicio 1.2
    -Se modificó el campo nombre por NOMBRE_SISTEMA, además, este ahora solo pertenece a la clase al ser static, y no se puede modificar al ser final.
    
    -Se agregó un método para imprimir el campo.

--- P2-TP1/src/SistemaGestor.java
+++ P2-TP1/src/SistemaGestor.java
@@ -3,9 +3,16 @@
  */
 public class SistemaGestor {
     // Atributos del sistema gestor
-    String nombre = "Gestor universitario";
+    /*  El campo nombre pertenece a la clase en sí, ya que,
+      está declarada como "static", su valor no puede cambiar porque está
+     declarado como "final"*/
+    public static final String NOMBRE_SISTEMA = "Gestor universitario";
     /*
     Bloque de configuración
     Aqui iran las variables y métodos próximamente
     */
+
+   public String imprimirNombre(){
+    return "El nombre del Sistema Gestor es: " + NOMBRE_SISTEMA;
+   }
 }
