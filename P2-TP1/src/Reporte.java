19aa60b14ab3b9497fda83dbaa44ea1695e41d76
Pablo Aguilar <paedmaag@gmail.com>
2026-04-21 02:19:54 -0300

    -m feat: se realizo el ejercicio 2.3
    -Se agregó el método varargs para agregar n cantidad de secciones, además, este posee un método for-each para imprimir n veces el/los string/s introducido/s.

--- P2-TP1/src/Reporte.java
+++ P2-TP1/src/Reporte.java
@@ -1,5 +1,5 @@
 public class Reporte {
-    
+
     //método sin retorno, este método imprime un texto en consola
     public void generarEncabezado(){
         System.out.println("Reporte generado!");
@@ -8,5 +8,16 @@ public class Reporte {
     //método con retorno, este método retorna una cadena de caracteres, retorna un String
     public String obtenerCuerpo(){
         return "Cuerpo de reporte obtenido";
+    }
+
+    //Metodo con parametros variables (varargs)
+    //Los 3 puntos indican que puede recibir n Strings)
+    public void procesadorSecciones(String... secciones){
+        System.out.println("Procesando "+ secciones.length + " secciones:");
+        //Recorre el arreglo de secciones con un for-each 
+        // y las imprime
+        for (String seccion: secciones){
+            System.out.println("- " + seccion);
+        }
     }
 }
