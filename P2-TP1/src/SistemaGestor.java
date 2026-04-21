82d06e63725988845d6e9c9330707107d7226068
Pablo Aguilar <paedmaag@gmail.com>
2026-04-21 00:44:49 -0300

    -m init: Ejercicio 1.1
    -Se creó la clase SistemaGestor con lo solicitado en el ejercicio 1.1.

---
+++ P2-TP1/src/SistemaGestor.java
@@ -0,0 +1,11 @@
+/**
+ * Clase "Sistema Gestor" que se encarga de gestionar cosas
+ */
+public class SistemaGestor {
+    // Atributos del sistema gestor
+    String nombre = "Gestor universitario";
+    /*
+    Bloque de configuración
+    Aqui iran las variables y métodos próximamente
+    */
+}
